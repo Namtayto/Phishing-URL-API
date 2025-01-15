5459d5bef24c2d07725882b02d51324dd560d591
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2025-01-15 09:51:58 +0700

    Edit prediction

--- API.py
+++ API.py
@@ -7,9 +7,9 @@ def get_prediction(url, model):
     # Use the isURLMalicious function to get the prediction
     prediction = Utils.isURLMalicious(url, model)
 
-    # Calculate the percentage
-    i = prediction[0][0] * 100
-    i = round(i, 3)
+    # # Calculate the percentage
+    # i = prediction[0][0] * 100
+    # i = round(i, 3)
 
-    print("There is", i, "% chance the URL is malicious!")
-    return i
+    print("There is", prediction, "% chance the URL is malicious!")
+    return prediction
